0f17ea003fd0fe885d9883f10501e8960d31b223
AndreLuizMMS <andreluiz.morais19@gmail.com>
2023-07-16 18:29:33 -0300

    fix: Logout Controller duplicate user

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -5,7 +5,7 @@ const usersDB = {
   }
 };
 
-const fsPromisses = require('fs/promises');
+const fsPromises = require('fs/promises');
 const path = require('path');
 
 const handleLogout = async (req, res) => {
@@ -16,24 +16,24 @@ const handleLogout = async (req, res) => {
 
   const refreshToken = cookies.jwt;
 
-  let foundUser = usersDB.users.filter(user => user.refreshToken == refreshToken);
+  const foundUser = usersDB.users.filter(user => user.refreshToken == refreshToken);
   if (!foundUser) {
-    res.clearCookie('jwt', { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
+    res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
     return res.sendStatus(204);
   }
 
-  const otherUsers = usersDB.users.filter(
-    user => user.refreshToken !== foundUser.refreshToken
-  );
-  foundUser = { ...foundUser, refreshToken: '' };
-  usersDB.setUsers([...otherUsers, foundUser]);
+  const otherUsers = usersDB.users.filter(user => {
+    return user.refreshToken !== refreshToken;
+  });
+  const currentUser = { ...foundUser[0], refreshToken: '' };
 
-  await fsPromisses.writeFile(
+  usersDB.setUsers([...otherUsers, currentUser]);
+  await fsPromises.writeFile(
     path.join(__dirname, '..', 'model', 'users.json'),
     JSON.stringify(usersDB.users)
   );
 
-  res.clearCookie('jwt', { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
+  res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
   res.sendStatus(204);
 };
 
